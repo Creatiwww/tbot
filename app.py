b32e17279211b0edd88be7b1fbad0abba07df74d
Vitaly <vitaly@MacBook-Pro-admin.local>
2020-04-10 02:56:29 +0300

    make it better

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, request
+import telegram
+import os
+
+TOKEN = os.environ['TOKEN']
+bot = telegram.Bot(token=TOKEN)
+URL = os.environ['URL']
+
+app = Flask(__name__)
+
+
+@app.route('/{}'.format(TOKEN), methods=['POST'])
+def respond():
+    update = telegram.Update.de_json(request.get_json(force=True), bot)
+
+    chat_id = update.message.chat.id
+    msg_id = update.message.message_id
+
+
+    # text = update.message.text.encode('utf-8').decode()
+
+    bot.sendMessage(chat_id=chat_id, text="Hi!", reply_to_message_id=msg_id)
+
+    return 'ok'
+
+if __name__ == '__main__':
+    app.run(threaded=True)
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+setup(
+    name="tbot",
+    packages=find_packages(),
+)
