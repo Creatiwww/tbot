3dca4a6cbf53757bd541dae4ee8600ff3de413df
Vitaly <vitaly@MacBook-Pro-admin.local>
2020-04-10 12:55:31 +0300

    make it better

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def respond():
     chat_id = update.message.chat.id
     msg_id = update.message.message_id
     # text = update.message.text.encode('utf-8').decode()
-    bot.sendMessage(chat_id=chat_id, text="Hi!", reply_to_message_id=msg_id)
+    bot.sendMessage(chat_id=chat_id, text="Hi there!", reply_to_message_id=msg_id)
     return 'ok'
 
 
