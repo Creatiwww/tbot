35cb47ffdc076b52f771fbf1871d8ff220b6aadf
Vitaly <vitaly@MacBook-Pro-admin.local>
2020-04-10 22:21:32 +0300

    make it better

--- app.py
+++ app.py
@@ -12,11 +12,18 @@ app = Flask(__name__)
 @app.route('/{}'.format(TOKEN), methods=['POST'])
 def respond():
     update = telegram.Update.de_json(request.get_json(force=True), bot)
+
     chat_id = update.message.chat.id
     msg_id = update.message.message_id
-    # text = update.message.text.encode('utf-8').decode()
-    bot.sendMessage(chat_id=chat_id, text="Hi there!", reply_to_message_id=msg_id)
-    return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
+
+    # Telegram understands UTF-8, so encode text for unicode compatibility
+    text = update.message.text.encode('utf-8').decode()
+    print("got text message :", text)
+
+    response = get_response(text)
+    bot.sendMessage(chat_id=chat_id, text=response, reply_to_message_id=msg_id)
+
+    return 'ok'
 
 
 @app.route('/', methods=['POST'])
