8e0863c49f4bd55d5dfc2c2cef6a4b77a26f70f0
Vitaly <vitaly@MacBook-Pro-admin.local>
2020-04-10 14:25:55 +0300

    make it better

--- app.py
+++ app.py
@@ -13,9 +13,14 @@ def respond():
     update = telegram.Update.de_json(request.get_json(force=True), bot)
     chat_id = update.message.chat.id
     msg_id = update.message.message_id
-    text = update.message.text.encode('utf-8').decode()
+    # text = update.message.text.encode('utf-8').decode()
     bot.sendMessage(chat_id=chat_id, text="Hi there!", reply_to_message_id=msg_id)
-    return TOKEN
+    return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
+
+
+@app.route('/')
+def index():
+    return json.dumps({'success':True}), 200, {'ContentType':'application/json'} 
 
 
 if __name__ == '__main__':
