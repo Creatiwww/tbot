98676226942b66eeb46d30b5677fb830b1b2632a
Vitaly <vitaly@MacBook-Pro-admin.local>
2020-04-10 13:44:34 +0300

    make it better

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def respond():
     update = telegram.Update.de_json(request.get_json(force=True), bot)
     chat_id = update.message.chat.id
     msg_id = update.message.message_id
-    # text = update.message.text.encode('utf-8').decode()
+    text = update.message.text.encode('utf-8').decode()
     bot.sendMessage(chat_id=chat_id, text="Hi there!", reply_to_message_id=msg_id)
     return 'ok'
 
