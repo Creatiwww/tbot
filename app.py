59c3c8fee0ea517a1a0c4946f39c389da114bab2
Vitaly <vitaly@MacBook-Pro-admin.local>
2020-04-11 02:08:48 +0300

    make it better

--- app.py
+++ app.py
@@ -2,40 +2,26 @@ from flask import Flask, request
 import telegram
 import os
 import json
-import logging
 
-global TOKEN 
-TOKEN = '1135698268:AAEipBbNjrsXeTQwjhf8M9TzJL0ADHAvFho'
-# TOKEN = os.environ['TOKEN']
-global bot 
+TOKEN = os.environ['TOKEN']
 bot = telegram.Bot(token=TOKEN)
-
-
 
 app = Flask(__name__)
 
 
 @app.route('/{}'.format(TOKEN), methods=['POST'])
 def respond():
-    print('enter respond fun')
     update = telegram.Update.de_json(request.get_json(force=True), bot)
-
     chat_id = update.message.chat.id
     msg_id = update.message.message_id
-
-    # Telegram understands UTF-8, so encode text for unicode compatibility
-    # mssg = update.message.text.encode('utf-8').decode()
-
-    # msg = update.message.text.encode('utf-8').decode()
-    bot.sendMessage(chat_id=chat_id, text="msg")
-
+    msg = update.message.text.encode('utf-8').decode()
+    bot.sendMessage(chat_id=chat_id, text=msg, reply_to_message_id=msg_id)
     return 'ok'
 
 
 @app.route('/', methods=['POST'])
 def index():
-    print('logss')
-    return json.dumps({'success':True}), 200, {'ContentType':'application/json'} 
+    return 'ok'
 
 
 if __name__ == '__main__':
